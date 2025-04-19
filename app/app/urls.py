c3a6e9b477b6bf1e0fd93c9e622c14233bb296dd
Pumidol <pumidol.l@gmail.com>
2025-04-19 12:52:31 +0000

    Refactor code structure for improved readability and maintainability

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,11 +19,11 @@ from appaccount.api import router as account_router
 from appdemo.api import router as demo_router  # remove when using this template
 from django.contrib import admin
 from django.urls import path
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Swagger
 
 from app import views
 
-api = NinjaAPI()
+api = NinjaAPI(docs=Swagger({"persistAuthorization": True}))
 api.add_router("/account", account_router)
 api.add_router("/demo", demo_router)  # remove when using this template
 
