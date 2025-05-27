a32f1b282af9fef039ffa00ac48866e5902e5984
Pumidol <pumidol.l@gmail.com>
2025-05-27 12:15:14 +0000

    Refactor code structure for improved readability and maintainability

--- app/appaccount/admin.py
+++ app/appaccount/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-from appaccount.models.accounts import User
+from appaccount.models.accounts import User, UserProfile
 
 admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile)
