cd85437257dee3a980a8a65b492ee806325ab97f
Pumidol <pumidol.l@gmail.com>
2025-07-02 10:35:29 +0000

    Improve error handling in load_env_vars function for missing .env file

--- .devcontainer/load_env_vars.fish
+++ .devcontainer/load_env_vars.fish
@@ -1,6 +1,10 @@
 # https://www.supplycode.dev/posts/fish-shell-script-to-source-enviorment-variables-from-an-env-file
 function load_env_vars -d "Load variables in a .env file"
     echo "Loading environment variables from $argv"
+    if not test -f $argv
+        echo "File not found: $argv"
+        return 1
+    end
     set lines (cat $argv | string split -n '\n' | string match -vre '^#')
     for line in $lines
         set arr (string split -n -m 1 = $line)
